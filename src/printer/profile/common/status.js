1a8f8e5b9d561e47b31b539a80ec0e4ede242c36
Alan-Liang <a@keeer.net>
2019-12-13 19:02:39 +0800

    refactor: use ipp to update status for common

--- src/printer/profile/common/status.js
+++ src/printer/profile/common/status.js
@@ -1,8 +1,21 @@
-const printer = require('node-native-printer')
+const ipp = require('ipp')
+const { promisify } = require('util')
+require('ipp/lib/parser').handleUnknownTag = () => {}
 
-module.exports = name => {
-  const info = printer.printerInfo(name)
-  if (info.jobs.some(job => job.status !== 'completed' && job.status !== 'canceled')) {
-    return { state: 'printing', message: null }
-  } else return { state: 'idle', message: null }
+module.exports = async name => {
+  const printer = ipp.Printer(`ipp://localhost:631/printers/${encodeURIComponent(name)}`)
+
+  const msg = {
+    'operation-attributes-tag': {
+      'which-jobs': 'all',
+      'requested-attributes': [ 'job-state' ],
+    },
+  }
+  const jobs = await promisify(printer.execute).call(printer, 'Get-Jobs', msg)
+  const hasJobPending = jobs['job-attributes-tag'].some(job => ['completed', 'canceled'].indexOf(job['job-state']) < 0)
+
+  let state = 'idle'
+  if (hasJobPending) state = 'printing'
+  
+  return { state, message: null }
 }
