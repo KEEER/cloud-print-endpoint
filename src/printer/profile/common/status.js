856ef5fa9d291530a67da9945e93dd03e51e421c
Alan-Liang <a@keeer.net>
2020-01-03 17:36:45 +0800

    feat(printer): device hp-m1136

--- src/printer/profile/common/status.js
+++ src/printer/profile/common/status.js
@@ -2,6 +2,11 @@ const ipp = require('ipp')
 const { promisify } = require('util')
 require('ipp/lib/parser').handleUnknownTag = () => {}
 
+const errorCodeMapping = {
+  'media-empty-error': 'out-of-paper',
+  'media-jam-error': 'paper-jam',
+}
+
 module.exports = async name => {
   const printer = ipp.Printer(`ipp://localhost:631/printers/${encodeURIComponent(name)}`)
 
@@ -12,10 +17,17 @@ module.exports = async name => {
     },
   }
   const jobs = await promisify(printer.execute).call(printer, 'Get-Jobs', msg)
-  const hasJobPending = jobs['job-attributes-tag'].some(job => ['completed', 'canceled'].indexOf(job['job-state']) < 0)
+  const hasJobPending = jobs['job-attributes-tag'].some(job => ['completed', 'canceled', 'processing-stopped', 'aborted'].indexOf(job['job-state']) < 0)
+  const attributes = await promisify(printer.execute).call(printer, 'Get-Printer-Attributes', null)
+  const reason = attributes['printer-attributes-tag']['printer-state-reasons']
+  const printerMessage = attributes['printer-attributes-tag']['printer-state-message']
 
-  let state = 'idle'
+  let state = 'idle', message = null
   if (hasJobPending) state = 'printing'
+  if (reason !== 'none') {
+    message = printerMessage
+    state = errorCodeMapping[reason] || 'unknown'
+  }
   
-  return { state, message: null }
+  return { state, message }
 }
