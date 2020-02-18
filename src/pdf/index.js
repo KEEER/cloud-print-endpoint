a82bc0e6af3285304c522dd209a7970dbc100ebc
Alan-Liang <a@keeer.net>
2020-02-18 13:46:45 +0800

    feat(pdf): replace hummus with pdf2json to reduce crash

--- src/pdf/index.js
+++ src/pdf/index.js
@@ -3,15 +3,23 @@ if(!file) {
   console.log('{"status":1,"message":"No file given"}')
   process.exit(1)
 }
-try {
-  console.log(JSON.stringify({
-    status: 0,
-    response: require('hummus').createReader(file).getPagesCount(),
-  }))
-} catch (e) {
-  console.log(JSON.stringify({
-    status: 1,
-    error: e.toString(),
-  }))
-  process.exit(1)
-}
+;(async () => {
+  try {
+    const pdfParser = new (require('pdf2json'))()
+    pdfParser.loadPDF(file)
+    const res = await new Promise((resolve, reject) => {
+      pdfParser.on('pdfParser_dataReady', resolve)
+      setTimeout(reject, 2000, 'timeout')
+    })
+    console.log(JSON.stringify({
+      status: 0,
+      response: res.formImage.Pages.length,
+    }))
+  } catch (e) {
+    console.log(JSON.stringify({
+      status: 1,
+      error: e.toString(),
+    }))
+    process.exit(1)
+  }
+})()
